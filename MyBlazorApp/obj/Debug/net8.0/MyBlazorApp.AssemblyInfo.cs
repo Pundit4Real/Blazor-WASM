8a8037d7371c444778ac4e7f81bff7179b740dce
Mohammed Ali <mohammedaalli088@gmail.com>
2024-06-01 17:46:13 +0000

    feat: ...

--- MyBlazorApp/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ MyBlazorApp/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1718c02f64c545da9057433d074bef5e556f50b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcac5372779355da57d2e712b28fc6f4f0521bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
